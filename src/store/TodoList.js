05c5015e82b49084d0a02d67cd62fa9c022f6eb0
Luísa Barros <qlullyq@gmail.com>
2019-05-30 20:48:54 -0300

    Configure ESLint

--- src/store/TodoList.js
+++ src/store/TodoList.js
@@ -1,17 +1,24 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import { connect } from "react-redux";
 
-const TodoList = props => {
-  console.log(props);
-  return (
-    <ul>
-      {props.todos.map(todo => (
-        <li key={todo.id}>{todo.text}</li>
-      ))}
-    </ul>
-  );
-};
+const TodoList = props => (
+  <ul>
+    {props.todos.map(todo => (
+      <li key={todo.id}>{todo.text}</li>
+    ))}
+  </ul>
+);
+
+// TodoList.propTypes = {
+//   todos: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       id: PropTypes.number,
+//       text: PropTypes.string
+//     })
+//   ).isRequired()
+// };
 
 const mapStateToProps = state => ({
   todos: state.todos
