9241c183b6a3a309b59bf1235e405b35e2a161e1
Luísa Barros <qlullyq@gmail.com>
2019-05-30 17:40:12 -0300

    Init repository

---
+++ src/store/TodoList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TodoList = () => (
+  <ul>
+    <li>Fazer café</li>
+    <li>Codar</li>
+    <li>Estudar</li>
+  </ul>
+);
+
+export default TodoList;
---
+++ src/store/reducers/todos.js
@@ -0,0 +1,1 @@
+export default function todos(state, action) {}
