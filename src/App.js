487ebeaa0dc28fe88842f6571649f8474ee9a3cc
Luísa Barros <qlullyq@gmail.com>
2019-06-03 16:11:26 -0300

    Configure Reactotron

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import "./config/ReactotronConfig";
+
 import { Provider } from "react-redux";
 
 import store from "./store";
 
 import TodoList from "./store/TodoList";
+
+console.tron.log("Oie");
 
 const App = () => (
   <Provider store={store}>
